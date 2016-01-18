92dafc7e9666a20b395b0f3d381f96398f4bbc96
haiv <hai_vu@mitel.com>
2016-01-18 15:03:50 -0500

    Corrected byteToString algorithm

--- src/org/dejavu/utils/LaCrypto.java
+++ src/org/dejavu/utils/LaCrypto.java
@@ -38,6 +38,10 @@ import javax.crypto.spec.SecretKeySpec;
  * @author hai
  */
 public class LaCrypto {
+	private static final Pattern gSaltPattern = Pattern.compile("^salt=(.+)$");
+	private static final Pattern gIvPattern = Pattern.compile("^iv\\s+=(.+)$");
+	private static final Pattern gKeyPattern = Pattern.compile("^key=(.+)$");
+	
 	private final char [] password;
 	/**
 	 * Key factory
@@ -65,12 +69,12 @@ public class LaCrypto {
 		this.password = password;
 	}
 	/**
-	 * Representing the encrypted data, and everything else required to decrypt it,
-	 * except for the password itself.
+	 * Representing the output data, and everything else required to decrypt it,
+ except for the password itself.
 	 */
 	public static final class EncryptedData {
 		/**
-		 * The encrypted data (optional)
+		 * The output data (optional)
 		 */
 		private byte [] encryptedData;
 		/**
@@ -90,7 +94,7 @@ public class LaCrypto {
 		 */
 		public final byte [] salt;
 		/**
-		 * Creates a new instance of encrypted data
+		 * Creates a new instance of output data
 		 * @param iters The iteration count
 		 * @param salt The 8-byte salt
 		 * @param iv The 16-byte initial vector
@@ -165,10 +169,10 @@ public class LaCrypto {
 	}
 	/**
 	 * Encrypts some data, in its entirety.
-	 * @param data The byte array containing the data to be encrypted
+	 * @param data The byte array containing the data to be output
 	 * @param salt The 8-byte salt
 	 * @param iterations The number of iterations for hashing
-	 * @return The encrypted data.
+	 * @return The output data.
 	 * @throws LaCryptoException 
 	 */
 	public EncryptedData encrypt(byte [] data, byte [] salt, int iterations) throws LaCryptoException {
@@ -188,9 +192,9 @@ public class LaCrypto {
 		}
 	}
 	/**
-	 * Decrypts some set of encrypted data
-	 * @param data The encrypted data
-	 * @return The decrypted data.
+	 * Decrypts some set of output data
+	 * @param data The output data
+	 * @return The output data.
 	 * @throws LaCryptoException 
 	 */
 	public byte [] decrypt(EncryptedData data) throws LaCryptoException {
@@ -210,8 +214,8 @@ public class LaCrypto {
 	
 	/**
 	 * Encrypts a stream of data, e.g. a file, to another stream, e.g. another file.
-	 * @param clearText The stream of clear-text data to be encrypted
-	 * @param encrypted The stream to write out the encrypted data
+	 * @param clearText The stream of clear-text data to be output
+	 * @param encrypted The stream to write out the output data
 	 * @param salt The salt to be used
 	 * @param iterations The iteration count to be used
 	 * @return The initialization vector generated during the encryption process.
@@ -246,8 +250,8 @@ public class LaCrypto {
 	
 	/**
 	 * Decrypts a stream of data, e.g. a file, to another stream, e.g. another file.
-	 * @param encrypted The stream of encrypted data to be decrypted
-	 * @param clearText The stream to write out the decrypted data
+	 * @param encrypted The stream of output data to be output
+	 * @param clearText The stream to write out the output data
 	 * @param salt The salt to be used
 	 * @param iv The initialisation vector to be used in the decryption process.
 	 * @param iterations The iteration count to be used
@@ -267,8 +271,8 @@ public class LaCrypto {
 	
 	/**
 	 * Decrypts a stream of data, e.g. a file, to another stream, e.g. another file.
-	 * @param encrypted The stream of encrypted data to be decrypted
-	 * @param clearText The stream to write out the decrypted data
+	 * @param encrypted The stream of output data to be output
+	 * @param clearText The stream to write out the output data
 	 * @param key The key
 	 * @param iv The initialisation vector to be used in the decryption process.
 	 * @throws LaCryptoException 
@@ -297,23 +301,36 @@ public class LaCrypto {
 		}
  	}
 	
-	private static final Pattern gSaltPattern = Pattern.compile("^salt=(.+)$");
-	private static final Pattern gIvPattern = Pattern.compile("^iv\\s+=(.+)$");
-	private static final Pattern gKeyPattern = Pattern.compile("^key=(.+)$");
+	/**
+	 * Converts a hex string into the equivalent byte array. E.g.
+	 * "AB1C66" -> [0xab, 0x1c, 0x66]
+	 * If a string containing non numerical values is given, some runtime exception
+	 * (most probably NumberFormatException) will be thrown.
+	 * @param str The string containing hex byte values to be converted
+	 * @return The byte array associated with the given string.
+	 */
 	private static byte [] stringToBytes(String str) {
 		int begin = 0;
 		int end = begin + 2;
 		int len = str.length();
 		byte[] ret = new byte[len/2];
 		int idx = 0;
-		while(end < len) {
+		while(end <= len) {
 			ret[idx++] = (byte)Integer.parseInt(str.substring(begin, end), 16);
 			begin += 2;
 			end += 2;
 		}
 		return ret;
 	}
-	
+	/**
+	 * Retrieves the encryption parameters, e.g. key, salt, and IV, from a file
+ that was output using OpenSSL with a password.
+	 * @param encrypted The OpenSSL-output file
+	 * @param password The password with which the file was output
+	 * @return The encryption information.
+	 * @throws LaCryptoException
+	 * @throws InterruptedException 
+	 */
 	public static EncryptedData getEncryptedDataFromOpenSsl(File encrypted, char [] password) throws LaCryptoException, InterruptedException {
 		try {
 			ProcessBuilder cmd = new ProcessBuilder(new String[]{"openssl", "enc", "-aes-256-cbc", "-d", "-pass", "pass:" + String.valueOf(password), "-P", "-in", encrypted.toString()});
@@ -363,10 +380,48 @@ public class LaCrypto {
 			throw new LaCryptoException(ex);
 		}
 	}
-	
+	/**
+	 * Decrypts a file (that was output with OpenSSL) using OPenSSL.
+	 * @param encrypted The output file.
+	 * @param decrypted The file containing output information.
+	 * @param password The password to be used for decryption
+	 * @throws LaCryptoException
+	 * @throws InterruptedException 
+	 */
 	public static void decryptOpenSslFile(File encrypted, File decrypted, char [] password) throws LaCryptoException, InterruptedException {
 		try {
 			ProcessBuilder cmd = new ProcessBuilder(new String[]{"openssl", "enc", "-aes-256-cbc", "-d", "-pass", "pass:" + String.valueOf(password), "-out", decrypted.toString(), "-in", encrypted.toString()});
+			Process proc = cmd.start();
+			try {
+				InputStream is = proc.getInputStream();
+				try {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+					String line;
+					while ((line = reader.readLine()) != null) {
+						System.out.println(line);
+					}
+				} finally {
+					is.close();
+				}
+			} finally {
+				proc.waitFor();
+			}
+		} catch (IOException ex) {
+			throw new LaCryptoException(ex);
+		}
+	}
+	
+	/**
+	 * Encrypts a file using OPenSSL.
+	 * @param plainText The plain text file to be output.
+	 * @param encrypted The file containing the resulting output data.
+	 * @param password The password to be used for encryption
+	 * @throws LaCryptoException
+	 * @throws InterruptedException 
+	 */
+	public static void encryptFileOpenSsl(File plainText, File encrypted, char [] password) throws LaCryptoException, InterruptedException {
+		try {
+			ProcessBuilder cmd = new ProcessBuilder(new String[]{"openssl", "enc", "-aes-256-cbc", "-salt", "-pass", "pass:" + String.valueOf(password), "-out", encrypted.toString(), "-in", plainText.toString()});
 			Process proc = cmd.start();
 			try {
 				InputStream is = proc.getInputStream();
@@ -393,59 +448,78 @@ public class LaCrypto {
 			String password = "ashdkjahdk";
 			String data = "This is a test";
 			int iters = 0xffff;
+			
+			// Encrypt small data block
 			LaCrypto crypt = new LaCrypto(password.toCharArray());
-			EncryptedData enc = crypt.encrypt(data.getBytes(), salt, iters);
-			System.out.printf("Encrypted = %s\n", enc);
-			LaCrypto dec = new LaCrypto(password.toCharArray());
-			byte[] d = dec.decrypt(enc);
+			EncryptedData encryptedData = crypt.encrypt(data.getBytes(), salt, iters);
+			System.out.printf("Encrypted = %s\n", encryptedData);
+			
+			// Decrypt small data block
+			crypt = new LaCrypto(password.toCharArray());
+			byte[] d = crypt.decrypt(encryptedData);
 			System.out.printf("From '%s' to '%s'\n", data, new String(d));
-			InputStream clear = new FileInputStream(new File("build.xml"));
+			
+			File enc1 = new File("enc1.xml");
+			File dec1 = new File("dec1.xml");
 			byte[] iv = null;
+			
+			// Encrypt file using JCE
+			InputStream clear = new FileInputStream(new File("build.xml"));
 			try {
-				OutputStream encrypted = new FileOutputStream(new File("build-enc.xml"));
+				OutputStream output = new FileOutputStream(enc1);
 				try {
 					LaCrypto krypt = new LaCrypto(password.toCharArray());
-					iv = krypt.encrypt(clear, encrypted, salt, iters);
+					iv = krypt.encrypt(clear, output, salt, iters);
 				} finally {
-					encrypted.close();
+					output.close();
 				}
 			} finally {
 				clear.close();
 			}
-			InputStream encrypted = new FileInputStream(new File("build-enc.xml"));
+			
+			// Decrypt file using JCE
+			InputStream input = new FileInputStream(enc1);
 			try {
-				OutputStream decrypted = new FileOutputStream(new File("build-dec.xml"));
+				OutputStream output = new FileOutputStream(dec1);
 				try {
 					LaCrypto krypt = new LaCrypto(password.toCharArray());
-					krypt.decrypt(encrypted, decrypted, salt, iv, iters);
+					krypt.decrypt(input, output, salt, iv, iters);
 				} finally {
-					decrypted.close();
+					output.close();
 				}
 			} finally {
-				encrypted.close();
+				input.close();
 			}
-			File openSslFile = new File("build-openssl.xml");
-			File closeSslFile = new File("build-closessl.xml");
-			EncryptedData encData = getEncryptedDataFromOpenSsl(openSslFile, "password".toCharArray());
-			System.out.printf("Found %s from %s\n", encData, openSslFile);
-			LaCrypto krypt = new LaCrypto("password".toCharArray());
-			InputStream openSsl = new FileInputStream(openSslFile);
+			
+			// Encrypt file using OpenSSL
+			File enc2 = new File("enc2.xml");
+			encryptFileOpenSsl(new File("build.xml"), enc2, password.toCharArray());
+			
+			// Decrypt the file that was encrypted by OpenSSL with JCE with some help from OpenSSH
+			File dec2 = new File("dec2.xml");
+			File dec3 = new File("dec3.xml");
+			EncryptedData encData = getEncryptedDataFromOpenSsl(enc2, password.toCharArray());
+			System.out.printf("Found %s from %s\n", encData, enc2);
+			crypt = new LaCrypto(password.toCharArray());
+			input = new FileInputStream(enc2);
 			try {
 				// Skip salt
 				byte[] skip = new byte[16];
-				openSsl.read(skip);
-				OutputStream decc = new FileOutputStream(new File("test.xml"));
+				input.read(skip);
+				OutputStream output = new FileOutputStream(dec2);
 				try {
-					krypt.decrypt(openSsl, decc, encData.key, encData.iv);
+					crypt.decrypt(input, output, encData.key, encData.iv);
 				} catch(LaCryptoException ex) {
 					Logger.getLogger(LaCrypto.class.getName()).log(Level.SEVERE, null, ex);
 				} finally {
-					decc.close();
+					output.close();
 				}
 			} finally {
-				openSsl.close();
+				input.close();
 			}
-			LaCrypto.decryptOpenSslFile(openSslFile, closeSslFile, "password".toCharArray());
+			
+			// Decrypt the file that was encrypted with OpenSSL using OpenSSH
+			LaCrypto.decryptOpenSslFile(enc2, dec3, password.toCharArray());
 		} catch (IOException | LaCryptoException ex) {
 			Logger.getLogger(LaCrypto.class.getName()).log(Level.SEVERE, null, ex);
 		} catch (InterruptedException ex) {
